70d71baca3e07f4e661719794f13b322f75f610d
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-09-18 22:37:02 +0300

    Creating table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,20 @@
 "INSERT INTO students (Name,Age,Gender,points) VALUES ("Souheir",22,"Female",5)"
 "UPDATE students SET Points=Points+1 WHERE Name='Bassma'"
 "UPDATE students SET Points=Points-1 WHERE Name='Alex'"
+
+"CREATE TABLE graduates( 
+ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+Name TEXT NOT NULL UNIQUE,
+Age INTEGER,
+Gender TEXT,
+Points INTEGER,
+Graduation TEXT
+)"
+"INSERT INTO graduates (Name,Age,Gender,Points)
+SELECT Name,Age,Gender,Points FROM students WHERE Name='Layal'"
+
+"UPDATE graduates
+SET Graduation='18/10/2020'
+WHERE Name='Layal';"
+
+"DELETE FROM students WHERE Name='Layal';"
